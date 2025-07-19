4355e60a9b6d0dfc639abfed2a103237e68aad5c
Raju Sakshena <jackmyble@gmail.com>
2025-07-19 20:19:31 +0530

    Delete fix_certificates.py

--- fix_certificates.py
+++
@@ -1,6 +0,0 @@
-import ssl
-import certifi
-
-print("✅ Installing certificates from certifi...")
-ssl_context = ssl.create_default_context(cafile=certifi.where())
-print("✅ Certificate installation completed.")
